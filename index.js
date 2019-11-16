e179146bd173638f733498af55d52e6116f1a046
Monica  Karasek <mlkarasek@gmail.com>
2019-11-16 23:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
-appemd.push("Odie");
+append.push("Odie");
 
 const prepend = ["Milo", "Otis", "Garfield"];
 prepend.unshift("Odie");
